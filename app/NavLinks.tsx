148f051c270cf9b2fc04e9d065441427390ae4b3
Punith Nandiraj <punithnandiraj@gmail.com>
2023-02-27 00:07:10 +0530

    Created Header , Working on Navlinks

---
+++ app/NavLinks.tsx
@@ -0,0 +1,12 @@
+import { categories } from "../constants"
+import NavLink from "./NavLink"
+
+function NavLinks() {
+  return <nav>
+    {categories.map((category) => (
+        <NavLink key={category} category={category} isActive={true} />
+    ))}
+  </nav>
+}
+
+export default NavLinks
